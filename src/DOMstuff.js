5e75fed8a61cd812678120a97f199745cc1d0b0e
tastas000 <TiiLg0o000@gmail.com>
2023-11-18 09:33:30 +0700

    Clean code

--- src/DOMstuff.js
+++ src/DOMstuff.js
@@ -1,132 +1,153 @@
 export default class DOMstuff {
-  static loadProject(projectObject) {
-    this.#clearMainSection();
-    this.#changeTabNameTo(projectObject.name);
+  static loadProjectList() {}
+  static loadTaskList(projectObject) {
+    clearMainSection();
+    changeTabNameTo(projectObject.name);
     const task_list = document.createElement("ul");
     task_list.id = "task-list";
 
     for (let taskObject of projectObject.tasks) {
-      console.log(taskObject);
-      const task = document.createElement("li");
-      task.classList.add("task");
-
-      const task_name = document.createElement("p");
-      task_name.classList.add("task_name");
-      task_name.textContent = taskObject.name;
-
-      const project_name = document.createElement("p");
-      project_name.classList.add("project-name");
-      if (projectObject.name === "common") {
-        project_name.textContent = "";
-      } else project_name.textContent = projectObject.name;
-
-      const description = document.createElement("button");
-      description.classList.add("description");
-      description.textContent = "description";
-      //Add description event listener
-
-      const due_date = document.createElement("p");
-      due_date.classList.add("due-date");
-      due_date.textContent = taskObject.dueDate;
-
-      const edit = document.createElement("button");
-      edit.classList.add("edit");
-      edit.textContent = "Edit";
-      //Add edit event listener
-
-      const remove = document.createElement("button");
-      remove.classList.add("remove");
-      remove.textContent = "remove";
-      //Add remove event listener
-
-      task.appendChild(task_name);
-      task.appendChild(project_name);
-      task.appendChild(description);
-      task.appendChild(due_date);
-      task.appendChild(edit);
-      task.appendChild(remove);
-
+      const task = createTask(taskObject, projectObject);
       task_list.appendChild(task);
     }
 
     const main_section = document.querySelector("#main-section");
     main_section.appendChild(task_list);
-    main_section.appendChild(this.#loadTaskAdder(projectObject));
+    main_section.appendChild(createTaskAdder(projectObject));
   }
+}
 
-  static #clearMainSection() {
-    const main_section = document.querySelector("#main-section");
-    while (main_section.childElementCount > 1) {
-      main_section.removeChild(main_section.lastChild);
-    }
+function clearMainSection() {
+  const main_section = document.querySelector("#main-section");
+  while (main_section.childElementCount > 1) {
+    main_section.removeChild(main_section.lastChild);
   }
-  static #changeTabNameTo(tabName) {
-    document.querySelector("#tab-name").textContent = tabName;
-  }
+}
 
-  static #loadTaskAdder(projectObject) {
-    const task_adder = document.createElement("div");
-    task_adder.id = "task-adder";
+function changeTabNameTo(tabName) {
+  document.querySelector("#tab-name").textContent = tabName;
+}
 
-    const add_task_button = document.createElement("button");
-    add_task_button.id = "add-task";
-    add_task_button.textContent = "+ Add task";
-    add_task_button.addEventListener("click", () => {
-      add_task_button.classList.add("hide");
-      add_task_form.classList.remove("hide");
-      name_input.focus();
-    });
+function createTask(taskObject, projectObject) {
+  const task = document.createElement("li");
+  task.classList.add("task");
 
-    const add_task_form = document.createElement("form");
-    add_task_form.id = "task-form";
-    add_task_form.classList.add("hide");
+  const task_name = document.createElement("p");
+  task_name.classList.add("task_name");
+  task_name.textContent = taskObject.name;
 
-    const name_input = document.createElement("input");
-    name_input.type = "text";
-    name_input.id = "name";
-    name_input.placeholder = "Task name";
-    name_input.required = true;
-    name_input.autofocus = true;
-    const description_input = document.createElement("input");
-    description_input.type = "text";
-    description_input.id = "description";
-    description_input.placeholder = "Task description";
-    description_input.required = true;
-    const date_input = document.createElement("input");
-    date_input.type = "date";
-    date_input.id = "date";
-    date_input.required = true;
-    const confirm_button = document.createElement("button");
-    confirm_button.type = "submit";
-    confirm_button.id = "confirm-task";
-    confirm_button.textContent = "Add";
-    const cancel_button = document.createElement("button");
-    cancel_button.type = "button";
-    cancel_button.id = "cancel-task";
-    cancel_button.textContent = "Cancel";
+  const project_name = document.createElement("p");
+  project_name.classList.add("project-name");
+  if (projectObject.name === "common") {
+    project_name.textContent = "";
+  } else project_name.textContent = projectObject.name;
 
-    add_task_form.addEventListener("submit", () => {
-      projectObject.addTask(
-        name_input.value,
-        description_input.value,
-        date_input.value
-      );
-      this.loadProject(projectObject);
-    });
+  const description = document.createElement("button");
+  description.classList.add("description");
+  description.textContent = "description";
+  //Add description event listener
 
-    cancel_button.addEventListener("click", () => {
-      this.loadProject(projectObject);
-    });
+  const due_date = document.createElement("p");
+  due_date.classList.add("due-date");
+  due_date.textContent = taskObject.dueDate;
 
-    add_task_form.appendChild(name_input);
-    add_task_form.appendChild(description_input);
-    add_task_form.appendChild(date_input);
-    add_task_form.appendChild(confirm_button);
-    add_task_form.appendChild(cancel_button);
+  const edit = document.createElement("button");
+  edit.classList.add("edit");
+  edit.textContent = "Edit";
+  //Add edit event listener
 
-    task_adder.appendChild(add_task_button);
-    task_adder.appendChild(add_task_form);
+  const remove = document.createElement("button");
+  remove.classList.add("remove");
+  remove.textContent = "remove";
+  remove.addEventListener("click", () => {
+    task.parentElement.removeChild(task);
+  });
 
-    return task_adder;
-  }
+  task.appendChild(task_name);
+  task.appendChild(project_name);
+  task.appendChild(description);
+  task.appendChild(due_date);
+  task.appendChild(edit);
+  task.appendChild(remove);
+
+  return task;
+}
+
+function createTaskAdder(projectObject) {
+  const task_adder = document.createElement("div");
+  task_adder.id = "task-adder";
+
+  const add_task_button = document.createElement("button");
+  add_task_button.id = "add-task";
+  add_task_button.textContent = "+ Add task";
+  add_task_button.addEventListener("click", () => {
+    add_task_button.classList.add("hide");
+    add_task_form.classList.remove("hide");
+    name_input.focus();
+  });
+
+  const add_task_form = document.createElement("form");
+  add_task_form.id = "task-form";
+  add_task_form.classList.add("hide");
+  
+
+  const name_input = document.createElement("input");
+  name_input.type = "text";
+  name_input.id = "name";
+  name_input.placeholder = "Task name";
+  name_input.required = true;
+  name_input.autofocus = true;
+  const description_input = document.createElement("input");
+  description_input.type = "text";
+  description_input.id = "description";
+  description_input.placeholder = "Task description";
+  description_input.required = true;
+  const date_input = document.createElement("input");
+  date_input.type = "date";
+  date_input.id = "date";
+  date_input.required = true;
+  const confirm_button = document.createElement("button");
+  confirm_button.type = "submit";
+  confirm_button.id = "confirm-task";
+  confirm_button.textContent = "Add";
+  const cancel_button = document.createElement("button");
+  cancel_button.type = "button";
+  cancel_button.id = "cancel-task";
+  cancel_button.textContent = "Cancel";
+
+  add_task_form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    projectObject.addTask(
+      name_input.value,
+      description_input.value,
+      date_input.value
+    );
+    const task = createTask(
+      projectObject.tasks[projectObject.tasks.length - 1],
+      projectObject
+    );
+    document.querySelector("#task-list").appendChild(task);
+    task_adder.parentElement.removeChild(task_adder);
+    document
+      .querySelector("#main-section")
+      .appendChild(createTaskAdder(projectObject));
+  });
+
+  cancel_button.addEventListener("click", () => {
+    task_adder.parentElement.removeChild(task_adder);
+    document
+      .querySelector("#main-section")
+      .appendChild(createTaskAdder(projectObject));
+  });
+
+  add_task_form.appendChild(name_input);
+  add_task_form.appendChild(description_input);
+  add_task_form.appendChild(date_input);
+  add_task_form.appendChild(confirm_button);
+  add_task_form.appendChild(cancel_button);
+
+  task_adder.appendChild(add_task_button);
+  task_adder.appendChild(add_task_form);
+
+  return task_adder;
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const project1 = new Project('project1');
 project1.addTask('123','456', (new Date()).toLocaleDateString());
 project1.addTask('1233','456', (new Date()).toLocaleDateString());
 project1.addTask('12333','456', (new Date()).toLocaleDateString());
-
 projectList.push(project1);
 
-DOMstuff.loadProject(project1);
+DOMstuff.loadTaskList(project1);
